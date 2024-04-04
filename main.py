f2c40b9245d763110c392407eaba756e40aad121
Zarin Karanikolov <86721477+zarkobeats@users.noreply.github.com>
2024-04-04 10:04:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,13 @@ from discord.ext import commands
 import asyncio
 import discord
 import datetime
-import requests
 
-
-# ТРЯБВА ДА ДОБАВЯ EMBED ЗА СЛЕДВАЩА ПЕСЕН
 
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
 queue = []
-
-offensive_words = ['дебел', 'грозен', 'тъп']
 
 
 client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
@@ -36,30 +31,30 @@ class MyView(discord.ui.View):
     @discord.ui.button(label="⏭️", style=discord.ButtonStyle.success)
     async def skip_button(self, interaction: discord.Interaction, button=discord.ui.Button):
         self.voice_client.stop()
-        await interaction.response.send_message("Следваща песен")
+        await interaction.response.send_message("Next song")
 
     @discord.ui.button(label="⏸️", style=discord.ButtonStyle.grey)
     async def pause_button(self, interaction: discord.Interaction,  button=discord.ui.Button):
         if self.button_pressed:
-            await interaction.response.send_message("Песента вече е паузирана")
+            await interaction.response.send_message("already paused")
         else:
             self.voice_client.pause()
-            await interaction.response.send_message("Пауза")
+            await interaction.response.send_message("Pause")
 
     @discord.ui.button(label="▶️", style=discord.ButtonStyle.primary)
     async def resume_button(self, interaction: discord.Interaction, button=discord.ui.Button):
         if self.button_pressed:
-            await interaction.response.send_message("Песента вече тече")
+            await interaction.response.send_message("Already playing")
         else:
             self.voice_client.resume()
             self.button_pressed = True
-            await interaction.response.send_message("Пускане")
+            await interaction.response.send_message("Playing")
 
-    @discord.ui.button(label="Leave 卐", style=discord.ButtonStyle.red)
+    @discord.ui.button(label="Leave", style=discord.ButtonStyle.red)
     async def leave_button(self, interaction: discord.Interaction, button=discord.ui.Button):
         await self.voice_client.disconnect()
         await self.message.edit(view=None)
-        await interaction.response.send_message("Arrivederci")
+        await interaction.response.send_message("Bye")
 
 
 async def play_next_song(voice_client, text_channel, message):
@@ -69,7 +64,7 @@ async def play_next_song(voice_client, text_channel, message):
                                                                                       "-reconnect_streamed 1 "
                                                                                       "-reconnect_delay_max 10")
         if source is None:
-            await text_channel.send('Не можах да заредя аудиото. Моля опитайте отново.')
+            await text_channel.send('Can`t load audio, please try again!')
             await voice_client.disconnect()
             return
         song_duration = info.get('duration', 0)
@@ -79,11 +74,11 @@ async def play_next_song(voice_client, text_channel, message):
                                                                                    client.loop))
         thumbnail_url = info.get('thumbnail', None)
         embed = discord.Embed(title=f"**{info['title']}**",
-                              description=f"Господ изпрати {message.author.mention}, да пусне тази песен",
+                              description=f"{message.author.mention} requested this song",
                               color=discord.Color.blue())
         if song_duration:
             duration = datetime.timedelta(seconds=song_duration)
-            embed.add_field(name="Таз песен е ей толко дълга:", value=str(duration), inline=True)
+            embed.add_field(name="Length of the song:", value=str(duration), inline=True)
         if thumbnail_url:
             embed.set_thumbnail(url=thumbnail_url)
 
@@ -101,7 +96,7 @@ async def p(ctx, *, query):
     if not voice_client:
         voice = ctx.author.voice
         if not voice:
-            await ctx.send("няма никой във войс чата")
+            await ctx.send("Nobody in voice chat")
             return
         voice_client = await voice.channel.connect()
 
@@ -117,7 +112,7 @@ async def q(ctx):
     if len(queue) == 0:
         await ctx.send("Queue is empty.")
         return
-    embed = discord.Embed(title="Заредени песни", description="Списък:", color=discord.Color.red())
+    embed = discord.Embed(title="Queued songs", description="List:", color=discord.Color.red())
     for i, song in enumerate(queue):
         duration = song.get('duration')
         if duration is not None:
@@ -125,7 +120,7 @@ async def q(ctx):
             seconds = duration % 60
             duration_str = f"{minutes}:{seconds:02d}"
         else:
-            duration_str = "Непознато"
+            duration_str = "Unknown"
         embed.add_field(name=f"{i + 1}. {song['title']}", value=f"Duration: {duration_str}", inline=False)
 
     await ctx.send(embed=embed)
@@ -136,9 +131,9 @@ async def pause(ctx):
     voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_playing():
         voice_client.pause()
-        await ctx.reply("Пауза")
+        await ctx.reply("Pause")
     else:
-        await ctx.reply("Вече е паузирано")
+        await ctx.reply("Already paused")
 
 
 @client.command()
@@ -146,9 +141,9 @@ async def skip(ctx):
     voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_playing():
         voice_client.stop()
-        await ctx.reply("Следваща песен")
+        await ctx.reply("Next song")
     else:
-        await ctx.reply("Плейлистът е празен")
+        await ctx.reply("Empty Playlist")
 
 
 @client.command()
@@ -156,23 +151,20 @@ async def resume(ctx):
     voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
     if voice_client and voice_client.is_paused():
         voice_client.resume()
-        await ctx.reply("Музиката продължава")
+        await ctx.reply("Music continues")
     else:
-        await ctx.reply("Вече се възпроизвежда")
+        await ctx.reply("Already playing")
 
 
 @client.command()
 async def helpme(ctx):
-    help_embed = discord.Embed(title="Команди", description="Списък на командите:", color=discord.Color.red())
-    help_embed.add_field(name="!p 'линк'", value="Пуска песен от YouTube", inline=False)
-    help_embed.add_field(name="!pause", value="Паузира вървящата песен", inline=False)
-    help_embed.add_field(name="!resume", value="Продължава вървящата песен", inline=False)
-    help_embed.add_field(name="!skip", value="Прескача текущата песен и продължава със следващата в Queue-то",
+    help_embed = discord.Embed(title="Commands", description="List of commands:", color=discord.Color.red())
+    help_embed.add_field(name="!p 'link'", value="Plays a song", inline=False)
+    help_embed.add_field(name="!pause", value="Pause", inline=False)
+    help_embed.add_field(name="!resume", value="Resume", inline=False)
+    help_embed.add_field(name="!skip", value="Skip to next song",
                          inline=False)
-    help_embed.add_field(name="!q", value="Показва заредените песни в плейлиста", inline=False)
-    help_embed.add_field(name="weather", value="Показва температурата в реално време", inline=False)
-    help_embed.add_field(name="time", value="Показва колко е часа", inline=False)
-    help_embed.set_footer(text="По бай Тошево време нямаше такива Ботове")
+    help_embed.add_field(name="!q", value="Shows the queue", inline=False)
     await ctx.send(embed=help_embed)
 
 
@@ -182,7 +174,7 @@ async def stop(ctx):
     if voice_client and voice_client.is_playing():
         queue.clear()
         voice_client.stop()
-        await ctx.reply("Концерта свърши")
+        await ctx.reply("End of playlist")
     await voice_client.disconnect()
 
 
@@ -191,46 +183,5 @@ async def leave(ctx):
     voice_client = discord.utils.get(client.voice_clients, guild=ctx.guild)
     await voice_client.disconnect()
 
-url = "https://dad-jokes.p.rapidapi.com/random/joke"
 
-
-@client.event
-async def on_message(message):
-    if message.content == 'и ловец съм':
-        await message.channel.send('и рибаp съм')
-    if message.content == 'time':
-        now = datetime.datetime.now()
-        realtime = now.strftime('%H:%M')
-        await message.channel.send(f"Часът е {realtime}.")
-    if message.content == 'weather':
-        api_url = f'https://api.open-meteo.com/v1/forecast?latitude=42.70&longitude=23.32&current_weather=true&timezone=Europe%2FMoscow'
-        response = requests.get(api_url)
-        data = response.json()
-        if 'current_weather' in data:
-            temperature = data['current_weather']['temperature']
-            await message.channel.send(f"В момента е {temperature}°C навън")
-        else:
-            await message.channel.send('Sorry, I could not retrieve the weather data.')
-    for word in offensive_words:
-        if word in message.content.lower():
-            await message.delete()
-            await message.channel.send(f"{message.author.mention}, моля, не използвай отново груби думи")
-            return
-    await client.process_commands(message)
-    if message.content == "dad joke":
-        headers = {
-            "X-RapidAPI-Key": "55c599f48amshbc09d50aed85e7bp13be40jsna35d4003b358",
-            "X-RapidAPI-Host": "dad-jokes.p.rapidapi.com"
-        }
-
-        response = requests.request("GET", url, headers=headers)
-
-        data = response.json()["body"][0]
-        punchline = data["punchline"]
-
-        response_text = f"{data['setup']}\n\n||{punchline}||"
-
-        await message.channel.send(response_text)
-
-
-client.run('MTA5MDU5NzQyMjIxODA4NDM2Mg.GXT903.bVDTxFCdZ2TRnZQZ6ic64GT6rAK9vSuq9ywhd4')
+client.run('personal bot key')
